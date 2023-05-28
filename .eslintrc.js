cfee890ffa34bc29a64e4455f7ab57a937d82623
Mirko Romstadt <mirko.romstadt@invention-center.de>
2023-05-28 13:50:14 +0200

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  ignorePatterns: ['dist'],
+  overrides: [],
+  plugins: ['@typescript-eslint'],
+  rules: {},
+};
